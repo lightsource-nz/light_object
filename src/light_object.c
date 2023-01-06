5e11655a58fd05d905575352c89263bf960dcdc3
tendrils <tendrils@users.noreply.github.com>
2023-01-07 04:57:00 +1300

    fix varargs usage

--- src/light_object.c
+++ src/light_object.c
@@ -11,6 +11,7 @@
 
 #include <stdio.h>
 #include <string.h>
+#include <stdarg.h>
 
 void light_object_init(struct light_object *obj, struct lobj_type *type)
 {
@@ -27,26 +28,26 @@ void light_object_put(struct light_object *obj)
         atomic_fetch_sub(&obj->ref_count, 1);
 }
 
-int light_object_set_name(struct light_object *obj, uint8_t *format, ...)
-{
-        va_list vargs;
-        int retval;
-
-        va_start(format, vargs);
-        retval = light_object_set_name_va(obj, format, vargs);
-        va_end(vargs);
-
-        return retval;
-
-}
 static int light_object_set_name_va(struct light_object *obj, const uint8_t *format, va_list vargs)
 {
         if(!format || !format[0]) {
                 // TODO log empty name field error
                 return -1;
         }
-        vasnprintf(obj->id, LOM_OBJ_NAME_LENGTH, format, vargs);
+        vsnprintf(obj->id, LOM_OBJ_NAME_LENGTH, format, vargs);
         return 0;
+}
+int light_object_set_name(struct light_object *obj, uint8_t *format, ...)
+{
+        va_list vargs;
+        int retval;
+
+        va_start(vargs, format);
+        retval = light_object_set_name_va(obj, format, vargs);
+        va_end(vargs);
+
+        return retval;
+
 }
 static int light_object_add_internal(struct light_object *obj)
 {
@@ -73,8 +74,8 @@ int light_object_add(struct light_object *obj, struct light_object *parent,
         va_list vargs;
         int retval;
 
-        va_start(format, vargs);
-        retval = light_object_add_va(obj, format, vargs);
+        va_start(vargs, format);
+        retval = light_object_add_va(obj, parent, format, vargs);
         va_end(vargs);
 
         return retval;
